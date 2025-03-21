ca48ea119f281aae12388056fb491234134f09bc
Sugar-ai-website <ellajuan.sugar@gmail.com>
2025-03-22 02:44:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("joinWaitlist").addEventListener("click", function() {
+  window.location.href = "你的報名連結";
+});
+
+document.getElementById("joinWaitlist2").addEventListener("click", function() {
+  window.location.href = "你的報名連結";
+});
